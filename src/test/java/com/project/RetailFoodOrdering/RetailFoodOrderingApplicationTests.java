62129bb1c514aae239087ed3b06c467efcebf12d
banapuramlakshmikanthreddy-creator <banapuramlakshmikanthreddy@gmail.com>
2026-04-07 11:20:14 +0530

    added basic files and frontend

---
+++ src/test/java/com/project/RetailFoodOrdering/RetailFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RetailFoodOrdering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
